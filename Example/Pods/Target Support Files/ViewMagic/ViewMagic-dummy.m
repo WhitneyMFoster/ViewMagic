5ac46fb2e818991d371cd602226717cf95ab7be4
Whitney Foster <whitney.m.foster@gmail.com>
2016-09-29 12:12:01 -0700

    initial commit

---
+++ Example/Pods/Target Support Files/ViewMagic/ViewMagic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ViewMagic : NSObject
+@end
+@implementation PodsDummy_ViewMagic
+@end
---
+++ Example/Pods/Target Support Files/ViewMagic/ViewMagic-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ViewMagicVersionNumber;
+FOUNDATION_EXPORT const unsigned char ViewMagicVersionString[];
+
